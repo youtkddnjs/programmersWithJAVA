c51ea96d777415cbacfeb9a44a7941be28a222cc
youtkddnjs <youtkddnjs91@gmail.com>
2024-10-16 08:48:53 +0900

    Create Project

---
+++ programmers_study_project/src/programmers_study_project/main.java
@@ -0,0 +1,10 @@
+package programmers_study_project;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("hello");
+	}// main
+
+} // class main
